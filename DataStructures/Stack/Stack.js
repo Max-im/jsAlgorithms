d4312a85c0b4c87f149a746a28be7698ea6c50fe
Max-im <pogidaevmo@gmail.com>
2023-02-14 17:10:50 +0200

    add stack

---
+++ DataStructures/Stack/Stack.js
@@ -0,0 +1,21 @@
+class Stack {
+  constructor() {
+    this.last = null;
+  }
+
+  add(item) {
+    const prev = this.last;
+    const element = { prev, item };
+    this.last = element;
+  }
+
+  get() {
+    const element = this.last;
+    if (!element) return null;
+    this.last = element.prev;
+    return element.item;
+  }
+}
+
+
+module.exports = Stack;
---
+++ DataStructures/Stack/index.js
@@ -0,0 +1,12 @@
+const Stack = require('./Stack');
+
+// Usage
+const stack = new Stack();
+stack.add({ name: 'first' });
+stack.add({ name: 'second' });
+stack.add({ name: 'third' });
+
+console.dir(stack.get());
+console.dir(stack.get());
+console.dir(stack.get());
+console.dir(stack.get());
